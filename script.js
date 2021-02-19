d34dc3b2ad3f5241840413e6629b6415e325616c
hirokideguchi <dgchrk7@gmail.com>
2021-02-19 15:34:51 +0900

    btn scroll 追加

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-
-
-document.getElementById("toppage").addEventListener("click",  function(){
+// クリック　スクロール
+document.getElementById("btn-work").addEventListener("click",  function(){
+  window.scroll({
+    top: 726,
+    behavior: "smooth"
+  });
+});
+document.getElementById("btn-about").addEventListener("click",  function(){
+  window.scroll({
+    top: 1305,
+    behavior: "smooth"
+  });
+});
+document.getElementById("btn-toppage").addEventListener("click",  function(){
   window.scroll({
     top: 0,
     behavior: "smooth"
   });
+});
+
+
+
+window.addEventListener("scroll", function(){
+  let scroll = document.documentElement.scrollTop;
+  console.log(scroll);
+  document.getElementById("scroll").textContent = scroll;
+
+  // if(scroll > ){
+
+  // }
+
+
 });
