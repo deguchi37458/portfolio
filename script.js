57d115e427e236da79159670cabf1e7f15a009b5
hirokideguchi <dgchrk7@gmail.com>
2021-02-19 16:23:20 +0900

    btn scroll レスポンシブ

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-// クリック　スクロール
+// スクロールで出現
+window.addEventListener("scroll", function(){
+  let scroll = document.documentElement.scrollTop;
+  console.log(scroll);
+  document.getElementById("scroll").textContent = scroll; //JS編集用
+
+  //navの出現
+  if(scroll > 726){
+    document.querySelector("nav").classList.add("show");
+  }else{
+    document.querySelector("nav").classList.remove("show");
+  }
+});
+
+
+// ボタンスクロール
 document.getElementById("btn-work").addEventListener("click",  function(){
   window.scroll({
     top: 726,
     behavior: "smooth"
   });
 });
+
 document.getElementById("btn-about").addEventListener("click",  function(){
-  window.scroll({
-    top: 1305,
-    behavior: "smooth"
-  });
+  if(window.matchMedia('(max-width: 600px)').matches) {
+    window.scroll({
+      top: 2235,
+      behavior: "smooth"
+    });
+  }else if (window.matchMedia('(min-width: 600px)').matches) {
+    window.scroll({
+      top: 1305,
+      behavior: "smooth"
+    });
+  };
 });
+
 document.getElementById("btn-toppage").addEventListener("click",  function(){
   window.scroll({
     top: 0,
     behavior: "smooth"
   });
 });
-
-
-// // スクロールで出現
-// window.addEventListener("scroll", function(){
-//   let scroll = document.documentElement.scrollTop;
-//   console.log(scroll);
-//   document.getElementById("scroll").textContent = scroll; //JS編集用
-
-//   //navの出現
-//   if(scroll > 726){
-//     document.querySelector("nav").classList.add("show");
-//   }else{
-//     document.querySelector("nav").classList.remove("show");
-//   }
-// });
